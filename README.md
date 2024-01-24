9871f920c60440ba071375017c4508a0821a092f
cucoo <cucoo@gmail.com>
2024-01-24 23:21:48 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # sitesimilarity
 Tool to compare sites and discover real site IP's in order to bypass WAFs 
+
+# How does it work?
+
+This tool is based on Luke Stephens (hakluke) tool hakoriginfinder (see https://github.com/hakluke/hakoriginfinder). This tool accepts as input parameters, two files containing IP's or URL's and compares them using the Levenshtein algorithm. You can set the threshold for comparision (default is 0.9) to be more loose or thight and you can also specify the number of threads to use (default is 32).
+
+The main difference between this version and Lukes version is that this one deals with multiple site comparisions in 
+a single run and also caches already downloaded pages to reduce requests.
+
+# Usage
+
+Imagine that you have a list of websites all protected by reverse proxy WAF and you want to find the real IP's of those sites. 
+
+In this scenario one can first find a list of ips belonging to the company that owns the websites and compare the sites on those IP's with the website list, using sitesimilarity tool to acomplish that.
+
+The command has the following format:
+
+python3 sitesimilariy.py file1 file2 [threshold] [threads] [debug]
+
+Where:
+
+    file1       : File containing for instance a list of ips to match
+    file2       : File containing for instance a list of urls to match
+    threshold   : Optional Levenshtein distance threshold, being a value between 0 (complete unmatch) and 1 
+                  (full match). The default value is 0.9.
+    threads     : Optional number of threads to distribute work. Default value is 32.
+    debug       : Optional debug parameter that shows aditional prints for debug purposes. Default is false
+
+Example: 
+
+```
+    python3 sitesimilarity.py ips.txt urls.txt 
+```
+
+# Output
+
+The output will consist of the input pairs that did match accordingly to the setted threshold.
+
+# Output example
+
+```
+    #python3 sitesimilarity.py ips.txt urls.txt
+
+    Match: 1.2.3.4 and www.url1.com
+    Match: 4.3.2.1 and www.url2.com
+
+```
+
+# Installation
+
+Just copy the python scritp to a location in your environment path.
---
+++ sitesimilarity.py
@@ -0,0 +1,147 @@
+#-------------------------------------------------------------------------------------
+# sitesimilariy - Tool to compare sites and discover real site IP's in order to bypass WAFs 
+#-------------------------------------------------------------------------------------
+# Author    : cucoo 
+# Twitter   : @cucooOnX    
+# Github    : cucooOnGitHub#
+#-------------------------------------------------------------------------------------
+# Description : This tool is based on Luke Stephens (hakluke) tool hakoriginfinder 
+#               (see https://github.com/hakluke/hakoriginfinder). This tool accepts as 
+#               input parameters, two files containing IP's or URL's and compares them
+#               using the Levenshtein algorithm. The main difference between this version 
+#               and Lukes version is that this one deals with multiple site comparisions in 
+#               a single run and also caches already downloaded pages to reduce requests.
+#
+# Example :
+#               python3 sitesimilarity.py ips.txt urls.txt
+#           
+#-------------------------------------------------------------------------------------
+import requests
+import Levenshtein
+import sys
+import concurrent.futures
+from urllib.parse import urlparse
+import logging
+from multiprocessing import Manager
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress warnings from urllib3 and requests libraries
+logging.captureWarnings(True)
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Cache to store fetched content
+manager=Manager()
+url_cache = manager.dict()
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            return content
+    except:
+        print(f"Error: Reading File '{filename}'")
+        sys.exit(1)
+
+def get_web_page(url, debug=False, ignore_ssl_errors=True):
+    if debug:
+        print(f"Fetching content from: {url}")
+
+    # Check if the content is already in the cache
+    if url in url_cache:
+        return url_cache[url]
+
+    try:
+        response = requests.get(url, verify=not ignore_ssl_errors)
+        response.raise_for_status()
+        content = response.text
+
+        # Store the content in the cache
+        url_cache[url] = content
+
+        return content
+    except:
+        # Log or handle the error silently
+        url_cache[url] = ""
+        return ''
+
+def calculate_similarity(text1, text2, threshold=0.9):
+    levenshtein_distance = Levenshtein.distance(text1, text2)
+    max_length = max(len(text1), len(text2))
+    similarity_ratio = 1 - levenshtein_distance / max_length
+
+    return similarity_ratio >= threshold
+
+def compare_urls(url1, url2, threshold, executor, debug=False):
+    content1 = get_web_page(url1, debug=debug)
+    content2 = get_web_page(url2, debug=debug)
+
+    if content1 is not None and content2 is not None:
+        is_similar = calculate_similarity(content1, content2, threshold)
+        if is_similar:
+            print(f"Match: '{url1}' and '{url2}'")
+
+def main():
+    # Check if two filenames are provided as command-line arguments
+    if len(sys.argv) != 3 and len(sys.argv) != 4 and len(sys.argv) != 5 and len(sys.argv) != 6:
+        print("Usage: python script.py file1.txt file2.txt [threshold] [num_threads] [debug]")
+        sys.exit(1)
+
+    # Redirect stderr to subprocess.DEVNULL to suppress errors
+    # sys.stderr = subprocess.DEVNULL
+
+    # Read the contents of the two files
+    file1_content = read_file(sys.argv[1])
+    file2_content = read_file(sys.argv[2])
+
+    # Split the content of each file into a list of URLs
+    file1_urls = file1_content.splitlines()
+    
+
+    file2_urls = file2_content.splitlines()
+
+    # Set the default threshold value
+    default_threshold = 0.9
+
+    # Check if a threshold value is provided as a command-line argument
+    if len(sys.argv) >= 4:
+        threshold = float(sys.argv[3])
+    else:
+        threshold = default_threshold
+
+    # Set the default number of threads
+    default_num_threads = 32
+
+    # Check if the number of threads is provided as a command-line argument
+    if len(sys.argv) >= 5:
+        num_threads = int(sys.argv[4])
+    else:
+        num_threads = default_num_threads
+
+    # Check if the debug flag is provided as a command-line argument
+    debug = len(sys.argv) == 6 and sys.argv[5].lower() == 'true'
+
+    if debug:
+        print("Debug mode: Printing all operations.")
+        print(f"Comparing with threshold {threshold} using {num_threads} threads.")
+
+    # Compare each combination of HTTP and HTTPS URLs in file1 against file2 using threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = []
+        for url1 in file1_urls:
+            for url2 in file2_urls:
+                # Create combinations of HTTP and HTTPS URLs 
+                if not url1 or not url2:
+                    continue 
+                combinations = [
+                    ("http://" + urlparse(url1).path, "http://" + urlparse(url2).path),
+                    ("https://" + urlparse(url1).path, "https://" + urlparse(url2).path)
+                ]
+
+                for http_url1, http_url2 in combinations:
+                    futures.append(executor.submit(compare_urls, http_url1, http_url2, threshold, executor, debug=debug))
+
+        # Wait for all tasks to complete
+        concurrent.futures.wait(futures)
+
+if __name__ == "__main__":
+    main()
